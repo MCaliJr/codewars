70771a60c36255a394ef7327781a83ad92a53327
mcalijr <martink.moc@gmail.com>
2021-11-18 23:32:27 +0100

    Something makes the page run super slow, still need to rework the code!

--- main.js
+++ main.js
@@ -2,6 +2,39 @@ function incrementString(strng) {
   let letters = strng.match(/[a-zA-Z]+/g); // "foo"
   let number = strng.match(/\d+/g); // "009"
   let lastNumber = number[0].charAt(number[0].length - 1);
+  let newArr = number[0].split("");
+
+  console.log(newArr[newArr.length - 1]);
+
+  if (lastNumber == 9) {
+    newArr[newArr.length - 1] = parseInt(newArr[newArr.length - 1]) + 1;
+    console.log(newArr[newArr.length - 1]);
+    // do {
+    //   newArr.forEach((element, index) => {
+    //     if (element == "10" || element == 10) {
+    //       newArr[index - 1] = Number(newArr[index - 1]) + 1;
+    //       element = 0;
+    //     }
+    //   });
+    // } while (newArr.includes(10 || "10"));
+  }
+
+  // split foo and 000
+
+  // make newArr array from 000, which is splitted by every digit
+
+  // if (number)
+
+  // if (lastNumber == 9)
+
+  // do while newArr contains "10" loop
+
+  // newArr.forEach((element, index) => {
+  //   if (element == "10") {
+  //     element = "0";
+  //     element[index - 1] = element[index - 1] + 1;
+  //   }
+  // });
 
   console.log(number[0].slice(0, -2));
   if (number) {

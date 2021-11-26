ce723e31ce2f0d52d019657d6937af2af3e2da48
mcalijr <martink.moc@gmail.com>
2021-11-26 20:52:23 +0100

    not much, still feeling sick

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-function findNb(m) {
-  let currentM = 0;
-  let n = 0;
+function race(v1, v2, g) {
+  if (v1 >= v2) return null;
+  v1sec = v1 / 3600;
+  v1DistAtGivensec = g;
+  v2sec = v2 / 3600;
+  v2DistAtGivensec = 0;
+  secsPassed = -1;
 
-  while (currentM < m) {
-    n += 1;
-    currentM += n ** 3;
+  while (v2DistAtGivensec <= v1DistAtGivensec) {
+    v1DistAtGivensec += v1sec;
+    v2DistAtGivensec += v2sec;
+    secsPassed++;
   }
 
-  return currentM === m ? n : -1;
+  let h = Math.floor(secsPassed / 3600);
+  let m = Math.floor((secsPassed % 3600) / 60);
+  let s = Math.floor((secsPassed % 3600) % 60);
+
+  return [h, m, s];
 }

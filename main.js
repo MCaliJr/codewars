99d7e901dbd0be6c4c9382eceea00aa3ba3b98a1
mcalijr <martink.moc@gmail.com>
2021-11-28 12:33:51 +0100

    First time use of recursion in JS, went pretty well but definitely need to work on that

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-function generateHashtag(str) {
-  strArr = str.split(" ");
-  strArr = strArr.map((word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  });
-  str = strArr.join("");
-  if (!str) return false;
-  str = `#${str}`;
-  if (str.length > 140) return false;
-  return str;
+function permutations(string) {
+  if (!string || typeof string !== "string") {
+    return;
+  } else if (string.length < 2) {
+    return [string];
+  }
+
+  let permutArr = [];
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+
+    if (string.indexOf(char) != i) continue;
+
+    let remainingChars =
+      string.slice(0, i) + string.slice(i + 1, string.length);
+
+    for (let permutation of permutations(remainingChars)) {
+      permutArr.push(char + permutation);
+    }
+  }
+  return permutArr;
 }
+
+string = "hello";
+console.log([string]);
